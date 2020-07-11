3ad9df842fb942a054bc484ebf97531c43390cd4
jpivard <62214998+jpivard@users.noreply.github.com>
2020-07-11 09:57:38 +0200

    Exemples de widgets

--- app.R
+++ app.R
@@ -9,7 +9,13 @@
 
 library(shiny)
 
-# # Define UI for application that makes a button
+#Ce fichier répertorie quelques exemples de widgets déjà construits. Il suffit de décommenter et d'éxecuter la partie qu'on veut tester.
+
+
+
+
+# 1. Action button
+
 # ui <- fluidPage(
 # 
 #     # Copy the line below to make an action button
@@ -31,3 +37,122 @@ library(shiny)
 # 
 # # Run the application 
 # shinyApp(ui = ui, server = server)
+
+
+#2. Checkbox group
+
+# ui <- fluidPage(
+#   
+#   # Copy the chunk below to make a group of checkboxes
+#   checkboxGroupInput("checkGroup", label = h3("Secteur"), 
+#                      choices = list("Transport" = 1, "Bâtiment" = 2, "Energie" = 3),
+#                      selected = 1),
+#   
+#   
+#   hr(),
+#   fluidRow(column(3, verbatimTextOutput("value")))
+#   
+# )
+# 
+# server <- function(input, output) {
+#   
+#   # You can access the values of the widget (as a vector)
+#   # with input$checkGroup, e.g.
+#   output$value <- renderPrint({ input$checkGroup })
+#   
+# }
+# 
+# shinyApp(ui = ui, server = server)
+
+
+#3.Date input
+
+# ui <- fluidPage(
+#   
+#   # Copy the line below to make a date selector 
+#   dateInput("date", label = h3("Date input"), value = "2014-01-01"),
+#   
+#   hr(),
+#   fluidRow(column(3, verbatimTextOutput("value")))
+#   
+# )
+# 
+# server <- function(input, output) {
+#   
+#   # You can access the value of the widget with input$date, e.g.
+#   output$value <- renderPrint({ input$date })
+#   
+# }
+# 
+# shinyApp(ui = ui, server = server)
+
+
+#4. Date range
+
+# ui <- fluidPage(
+#   
+#   # Copy the line below to make a date range selector
+#   dateRangeInput("dates", label = h3("Date range")),
+#   
+#   hr(),
+#   fluidRow(column(4, verbatimTextOutput("value")))
+#   
+# )
+# 
+# server <- function(input, output) {
+#   
+#   # You can access the values of the widget (as a vector of Dates)
+#   # with input$dates, e.g.
+#   output$value <- renderPrint({ input$dates })
+#   
+# }
+# 
+# shinyApp(ui = ui, server = server)
+
+
+# #5. File upload
+# 
+# ui <- fluidPage(
+#   
+#   # Copy the line below to make a file upload manager
+#   fileInput("file", label = h3("File input")),
+#   
+#   hr(),
+#   fluidRow(column(4, verbatimTextOutput("value")))
+#   
+# )
+# 
+# server <- function(input, output) {
+#   
+#   # You can access the value of the widget with input$file, e.g.
+#   output$value <- renderPrint({
+#     str(input$file)
+#   })
+#   
+# }
+# 
+# shinyApp(ui = ui, server = server)
+# 
+
+# #6. Text input
+# 
+# ui <- fluidPage(
+#   
+#   # Copy the line below to make a text input box
+#   textInput("text", label = h3("saisie de texte"), value = "Entrez votre texte..."),
+#   
+#   hr(),
+#   fluidRow(column(3, verbatimTextOutput("value")))
+#   
+# )
+# 
+# server <- function(input, output) {
+#   
+#   # You can access the value of the widget with input$text, e.g.
+#   output$value <- renderPrint({ input$text })
+#   
+# }
+# 
+# shinyApp(ui = ui, server = server)
+
+
